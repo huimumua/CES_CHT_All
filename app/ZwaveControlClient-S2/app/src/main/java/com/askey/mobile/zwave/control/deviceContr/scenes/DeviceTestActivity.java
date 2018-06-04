5317a5fba8990f90057c30742fdcc5ef6b0256bc
Leon_Deng <leon_deng@askey.com.tw>
2018-06-04 16:58:21 +0800

    modify remove/replace failed node UI

--- app/ZwaveControlClient-S2/app/src/main/java/com/askey/mobile/zwave/control/deviceContr/scenes/DeviceTestActivity.java
+++ app/ZwaveControlClient-S2/app/src/main/java/com/askey/mobile/zwave/control/deviceContr/scenes/DeviceTestActivity.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.askey.mobile.zwave.control.R;
@@ -23,6 +24,8 @@ import com.askey.mobile.zwave.control.deviceContr.localMqtt.MqttMessageArrived;
 import com.askey.mobile.zwave.control.deviceContr.model.ProvisionInfo;
 import com.askey.mobile.zwave.control.deviceContr.model.ScenesInfo;
 import com.askey.mobile.zwave.control.home.activity.addDevice.DeleteDeviceActivity;
+import com.askey.mobile.zwave.control.home.activity.addDevice.RemoveFailActivity;
+import com.askey.mobile.zwave.control.home.activity.addDevice.ReplaceFailActivity;
 import com.askey.mobile.zwave.control.home.adapter.CommandListAdapter;
 import com.askey.mobile.zwave.control.util.Const;
 import com.askey.mobile.zwave.control.util.Logg;
@@ -41,28 +44,38 @@ public class DeviceTestActivity extends BaseActivity {
     CommandListAdapter adapter;
     ProvisionInfo provisionInfo;
     List<String> interfaceClass;
-    String securityStatus,nodeId,homeId,deviceType;
-    private TextView homeIdTextView, nodeIdTextView, securityStatusTextView, deviceTypeTextView;
+    String securityStatus, nodeId, homeId, deviceType, deviceName;
+    private TextView homeIdTextView, nodeIdTextView, securityStatusTextView, deviceTypeTextView, title, nodeIsFailed;
+    private LinearLayout commandClassLayout, deviceNodeIsFailedLayout;
+    private Button remove, replace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_plug_test);
 
-        commList = (RecyclerView) findViewById(R.id.comm_list);
-        TextView title = (TextView)findViewById(R.id.command_class_title);
+        commandClassLayout = (LinearLayout)findViewById(R.id.command_class_layout);
+        deviceNodeIsFailedLayout = (LinearLayout)findViewById(R.id.failed_node_layout);
+        remove = (Button) findViewById(R.id.remove_failed_node);
+        replace = (Button) findViewById(R.id.replace_failed_node);
 
-        homeIdTextView = (TextView)findViewById(R.id.home_id);
-        nodeIdTextView = (TextView)findViewById(R.id.node_id);
-        securityStatusTextView = (TextView)findViewById(R.id.security_status);
-        deviceTypeTextView = (TextView)findViewById(R.id.device_type);
+        commList = (RecyclerView) findViewById(R.id.comm_list);
+        title = (TextView) findViewById(R.id.command_class_title);
+
+        homeIdTextView = (TextView) findViewById(R.id.home_id);
+        nodeIdTextView = (TextView) findViewById(R.id.node_id);
+        securityStatusTextView = (TextView) findViewById(R.id.security_status);
+        deviceTypeTextView = (TextView) findViewById(R.id.device_type);
+        nodeIsFailed = (TextView) findViewById(R.id.node_is_failed);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         commList.setLayoutManager(layoutManager);
-        title.setText(getIntent().getStringExtra("deviceName"));
 
+        deviceName = getIntent().getStringExtra("deviceName");
         nodeId = getIntent().getStringExtra("nodeId");
+
+        title.setText(String.format(getResources().getString(R.string.device_name_2), deviceName));
 
         interfaceClass = new ArrayList<>();
 
@@ -75,17 +88,39 @@ public class DeviceTestActivity extends BaseActivity {
 
             @Override
             public void onItemClick(View view, String str) {
-                Log.i("onItemClick", "====str="+str);
+                Log.i("onItemClick", "====str=" + str);
                 Intent intent = new Intent();
-                intent.setClass(mContext,APITestActivity.class);
-                intent.putExtra("title",str);
+                intent.setClass(mContext, APITestActivity.class);
+                intent.putExtra("title", str);
 
-                intent.putExtra("nodeId",getIntent().getStringExtra("nodeId"));
+                intent.putExtra("nodeId", getIntent().getStringExtra("nodeId"));
                 startActivity(intent);
             }
         });
 
-       getCmdList();
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, RemoveFailActivity.class);
+                intent.putExtra("nodeId", nodeId);
+                intent.putExtra("roomName", "MyHome");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        replace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, ReplaceFailActivity.class);
+                intent.putExtra("nodeId", nodeId);
+                intent.putExtra("roomName", "MyHome");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        getCmdList();
     }
 
     MqttMessageArrived mMqttMessageArrived = new MqttMessageArrived() {
@@ -98,7 +133,13 @@ public class DeviceTestActivity extends BaseActivity {
             if (result.contains("desired")) {
                 return;
             }
-            mqttMessageResult(result);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mqttMessageResult(result);
+                }
+            });
 
         }
     };
@@ -107,8 +148,8 @@ public class DeviceTestActivity extends BaseActivity {
         Log.d(TAG, "======getCmdList=======");
         MQTTManagement.getSingInstance().rigister(mMqttMessageArrived);
 
-        //MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.checkNodeIsFailed(nodeId));
-        MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.getSpecifyDeviceInfo(nodeId));
+        MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.checkNodeIsFailed(nodeId));
+        //MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.getSpecifyDeviceInfo(nodeId));
 
     }
 
@@ -130,13 +171,13 @@ public class DeviceTestActivity extends BaseActivity {
                     homeId = detailTmp.optString("Home id");
 
                     JSONArray endpointList = detailTmp.getJSONArray("EndPoint List");
-                    for (int j=0 ; j < endpointList.length();j++) {
+                    for (int j = 0; j < endpointList.length(); j++) {
                         JSONObject endpointTmp = endpointList.getJSONObject(j);
                         deviceType = endpointTmp.optString("ZWave+ device type"); //设备类型
                         JSONArray interfaceList = endpointTmp.getJSONArray("Interface List");
-                        for (int k=0; k < interfaceList.length(); k++) {
+                        for (int k = 0; k < interfaceList.length(); k++) {
                             JSONObject interfaceTmp = interfaceList.getJSONObject(k);
-                            Log.i(TAG, "--------------interface class:"+interfaceTmp.optString("Interface Class") );
+                            Log.i(TAG, "--------------interface class:" + interfaceTmp.optString("Interface Class"));
                             interfaceClass.add(interfaceTmp.optString("Interface Class"));
                         }
                     }
@@ -153,6 +194,18 @@ public class DeviceTestActivity extends BaseActivity {
                         adapter.notifyDataSetChanged();
                     }
                 });
+            } else if ("Node Is Failed Check Report".equals(messageType)) { //检查node所对应Device是否活着
+                String checkNodeStatus = reportedObject.optString("Status");
+                Log.i(TAG, "Node Is Failed Check Report Status:"+checkNodeStatus);
+                MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.getSpecifyDeviceInfo(nodeId));
+                //Alive 显示command class，Down显示replace按钮
+                if (checkNodeStatus.equals("Alive")) {  //Alive活着 / Down(failed)死了
+                    commandClassLayout.setVisibility(View.VISIBLE);
+                } else if (checkNodeStatus.equals("Down(failed)")) {
+                    nodeIsFailed.setVisibility(View.VISIBLE);
+                    deviceNodeIsFailedLayout.setVisibility(View.VISIBLE);
+                } else {
+                }
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -160,5 +213,3 @@ public class DeviceTestActivity extends BaseActivity {
     }
 
 }
-
-
