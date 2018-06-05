258f663d1a23444aa644aa37f938d2e5d138e12d
Leon_Deng <leon_deng@askey.com.tw>
2018-06-05 15:27:00 +0800

    修改checkNodeIsFailed时返回负数获取不到node info的情况

--- app/ZwaveControlClient-S2/app/src/main/java/com/askey/mobile/zwave/control/deviceContr/scenes/DeviceTestActivity.java
+++ app/ZwaveControlClient-S2/app/src/main/java/com/askey/mobile/zwave/control/deviceContr/scenes/DeviceTestActivity.java
@@ -1,29 +1,21 @@
 package com.askey.mobile.zwave.control.deviceContr.scenes;
 
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.askey.mobile.zwave.control.R;
 import com.askey.mobile.zwave.control.base.BaseActivity;
 import com.askey.mobile.zwave.control.data.LocalMqttData;
-import com.askey.mobile.zwave.control.deviceContr.adapter.CommandsAdapter;
 import com.askey.mobile.zwave.control.deviceContr.localMqtt.MQTTManagement;
 import com.askey.mobile.zwave.control.deviceContr.localMqtt.MqttMessageArrived;
 import com.askey.mobile.zwave.control.deviceContr.model.ProvisionInfo;
-import com.askey.mobile.zwave.control.deviceContr.model.ScenesInfo;
-import com.askey.mobile.zwave.control.home.activity.addDevice.DeleteDeviceActivity;
 import com.askey.mobile.zwave.control.home.activity.addDevice.RemoveFailActivity;
 import com.askey.mobile.zwave.control.home.activity.addDevice.ReplaceFailActivity;
 import com.askey.mobile.zwave.control.home.adapter.CommandListAdapter;
@@ -54,8 +46,8 @@ public class DeviceTestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_plug_test);
 
-        commandClassLayout = (LinearLayout)findViewById(R.id.command_class_layout);
-        deviceNodeIsFailedLayout = (LinearLayout)findViewById(R.id.failed_node_layout);
+        commandClassLayout = (LinearLayout) findViewById(R.id.command_class_layout);
+        deviceNodeIsFailedLayout = (LinearLayout) findViewById(R.id.failed_node_layout);
         remove = (Button) findViewById(R.id.remove_failed_node);
         replace = (Button) findViewById(R.id.replace_failed_node);
 
@@ -195,16 +187,21 @@ public class DeviceTestActivity extends BaseActivity {
                     }
                 });
             } else if ("Node Is Failed Check Report".equals(messageType)) { //检查node所对应Device是否活着
-                String checkNodeStatus = reportedObject.optString("Status");
-                Log.i(TAG, "Node Is Failed Check Report Status:"+checkNodeStatus);
                 MQTTManagement.getSingInstance().publishMessage(Const.subscriptionTopic, LocalMqttData.getSpecifyDeviceInfo(nodeId));
-                //Alive 显示command class，Down显示replace按钮
-                if (checkNodeStatus.equals("Alive")) {  //Alive活着 / Down(failed)死了
+                if (reported.contains("Status")) {
+                    String checkNodeStatus = reportedObject.optString("Status");
+                    Log.i(TAG, "Node Is Failed Check Report Status:" + checkNodeStatus);
+                    //Alive 显示command class，Down显示replace按钮
+                    if (checkNodeStatus.equals("Alive")) {  //Alive活着 / Down(failed)死了
+                        commandClassLayout.setVisibility(View.VISIBLE);
+                    } else if (checkNodeStatus.equals("Down(failed)")) {
+                        nodeIsFailed.setVisibility(View.VISIBLE);
+                        deviceNodeIsFailedLayout.setVisibility(View.VISIBLE);
+                    }
+                } else if (reported.contains("Error")) {
+                    int error = reportedObject.optInt("Error");
+                    Log.i(TAG, "Node Is Failed Check Report :" + error);
                     commandClassLayout.setVisibility(View.VISIBLE);
-                } else if (checkNodeStatus.equals("Down(failed)")) {
-                    nodeIsFailed.setVisibility(View.VISIBLE);
-                    deviceNodeIsFailedLayout.setVisibility(View.VISIBLE);
-                } else {
                 }
             }
         } catch (JSONException e) {
