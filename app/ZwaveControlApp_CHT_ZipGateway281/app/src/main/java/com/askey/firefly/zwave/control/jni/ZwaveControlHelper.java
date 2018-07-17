8f3e1d44e7d42e01371a958c8856278966f4b280
gino_kuo <gino_kuo@askey.com>
2018-07-17 11:34:44 +0800

    CHT update jni

--- app/ZwaveControlApp_CHT_ZipGateway281/app/src/main/java/com/askey/firefly/zwave/control/jni/ZwaveControlHelper.java
+++ app/ZwaveControlApp_CHT_ZipGateway281/app/src/main/java/com/askey/firefly/zwave/control/jni/ZwaveControlHelper.java
@@ -477,5 +477,6 @@ public class ZwaveControlHelper {
     public native static int ZwController_checkNodeIsFailed(int deviceId);
 
     public native static int ZwController_getSecurity2CmdSupported(int deviceId);
+    public native static int ZwController_sendNodeInformationFrame(int deviceId, int broadcastFlag);
 
 }
