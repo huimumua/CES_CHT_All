9f173f465b90198352caffdb03aa9f8a9c28907a
Leon_Deng <leon_deng@askey.com.tw>
2018-08-24 16:57:21 +0800

    solved CTT open error and application busy

--- app/ZwaveControlServer_ZipGateway281/app/src/main/cpp/zipgateway/src/DTLS_server.c
+++ app/ZwaveControlServer_ZipGateway281/app/src/main/cpp/zipgateway/src/DTLS_server.c
@@ -150,6 +150,39 @@ static void dtls_print_session_msg(const char* msg, struct dtls_session*s) {
     DBG_PRINTF("DTLS: %s %s %d %d\n", msg, inet_ntoa(s->connaddr.sin_addr), s->connaddr.sin_port, htons(s->connaddr.sin_port));
 }
 
+#define MAX_BIN_LINE_LEN    15
+void dump_buf_from_zware(void * buf, uint32_t len)
+{
+    uint32_t    line_len;
+    uint8_t     *bin_byte = (uint8_t *)buf;
+    char        tmp[8];
+    char        hex_str[MAX_BIN_LINE_LEN * 4];
+
+    while (len > 0)
+    {
+        if (len >= MAX_BIN_LINE_LEN)
+        {
+            len -= MAX_BIN_LINE_LEN;
+            line_len = MAX_BIN_LINE_LEN;
+        }
+        else
+        {
+            line_len = len;
+            len = 0;
+        }
+
+        hex_str[0] = '\0';
+
+        while (line_len-- > 0)
+        {
+            sprintf(tmp,"%02X ",(unsigned) *bin_byte++);
+            strcat(hex_str, tmp);
+        }
+
+        DBG_PRINTF("zipgateway:%s\n", hex_str);
+    }
+}
+
 static unsigned int
 psk_server_callback(SSL *ssl, const char *identity, unsigned char *psk,
                     unsigned int max_psk_len)
@@ -606,6 +639,15 @@ PROCESS_THREAD(dtls_server_process, ev, data)
 
                             BYTE nodeId = read_buf[0];
                             DBG_PRINTF("djnakata nodeId %d\n", nodeId);
+                            // DBG_PRINTF("*******Dump buffer from zware:\n");
+                            // dump_buf_from_zware(read_buf, len);
+                            // DBG_PRINTF("*******Dump buffer from zware done.\n");
+
+
+                            if(nodeId == 0)
+                            {
+                                nodeId = MyNodeID;
+                            }
 
                             int udp_payload_len = len - 1;
                             memset(&udpconn, 0x00, sizeof(struct uip_udp_conn));
@@ -643,7 +685,9 @@ PROCESS_THREAD(dtls_server_process, ev, data)
                             uip_ipaddr_copy(&UIP_IP_BUF->srcipaddr,  &udpconn.ripaddr);
                             uip_ipaddr_copy(&UIP_IP_BUF->destipaddr, &udpconn.sipaddr);
 
-                            if (nodeId == MyNodeID || nodeId == 1)
+                            DBG_PRINTF("djnakata MyNodeID is %d, nodeId is %d", MyNodeID, nodeId);
+
+                            if (nodeId == MyNodeID)
                             {
                                 uip_debug_ipaddr_print(&udpconn.ripaddr);
                                 uip_debug_ipaddr_print(&udpconn.sipaddr);
